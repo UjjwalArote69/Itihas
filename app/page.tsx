8bb6c51a1cf2b6a66a730093fda0ae143b25b24e
Ujjwal Arote <155384273+UjjwalArote69@users.noreply.github.com>
2026-01-28 16:37:39 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <section>
+            <h1>Itihas</h1>
+            <p>A visual exploration of human history</p>
+        </section>
+    )
+}
+export default Page
